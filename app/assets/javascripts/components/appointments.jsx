6ff7312bb7f80d34d52b20ae544db332343efbd0
Brendan O'Brien <titaniumcranium@icloud.com>
2018-08-11 15:56:10 +1000

    added appointments controller and index view, first React component with upgraded syntax

---
+++ app/assets/javascripts/components/appointments.jsx
@@ -0,0 +1,17 @@
+// var Appointments = React.createClass({
+//     render: function() {
+//         return (
+//             <h1>CalReact</h1>
+//         )
+//     }
+// })
+
+class Appointments extends React.Component{
+    render() {
+        return (
+            <h1>CalReact</h1>
+        )
+    }
+}
+
+
